86c6909d3a57d060f4e3b42293f13d0c7c55f07b
Oscar <41174627+oscisn93@users.noreply.github.com>
2022-08-30 23:38:49 -0700

    Update index.js
    included link to resource for later use

--- client/public/api/index.js
+++ client/public/api/index.js
@@ -4,6 +4,7 @@ const api = axios.create();
 
 /*
     TODO: create api calls to backend services
+    Uploading images: https://stackoverflow.com/questions/39663961/how-do-you-send-images-to-node-js-with-axios
 */
 
 export default {
