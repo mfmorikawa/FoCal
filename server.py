44a59e30edcab552245093eb0f3c379150e0759f
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-02 02:08:27 -0700

    moved server file to main directory

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(_name_)
+    app = Flask(__name__)
     
     @app.route('/')
     def index():
         return '<h1>index route</h1>'
-    
-    return app
+   
+    @app.route('/route_name')
+    def method_name():
+        pass
+        
+        return app
 
 # https://www.datasciencelearner.com/react-with-python-flask-api/
 # https://flask.palletsprojects.com/en/2.2.x/quickstart/#a-minimal-application
