7bce902c462ed7816c1359d03e4d690c657087d7
oscisn93 <oscar.cisneros@thelastmile.org>
2022-09-19 19:04:53 -0700

    removed unnecessary dependencies, updated path to build as that is the directory vite produces(dist is produced by create-react-app, which was abandoned in favor of vite)

--- app/routing/__init__.py
+++ app/routing/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 
 #? Unsure if I should really have the static_folder hardcoded here
-routing = Blueprint("routing", __name__, static_url_path="", static_folder="../../client/dist")
+routing = Blueprint("routing", __name__, static_url_path="", static_folder="../../client/build")
 
 from . import routes
