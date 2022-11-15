40e609964569ab5cb0c6988972a65fd2b0790d4c
Michael Morikawa <michael_f_morikawa@csu.fullerton.edu>
2022-11-14 17:36:08 -0800

    Update task schema to handle requested json shape

--- server/api/schemas/task.py
+++ server/api/schemas/task.py
@@ -1,10 +1,36 @@
-from marshmallow import Schema,fields
+from marshmallow import Schema, fields, post_load, post_dump
+from api.services.utils import DATE_FORMAT_STRING
+
+
+class ResourceSchema(Schema):
+    taskID = fields.UUID()
+    projectID = fields.UUID()
+    isCompleted = fields.Boolean()
+
 
 class TasksSchema(Schema):
-    objectID = fields.UUID()
     title = fields.Str()
-    start = fields.DateTime("%Y-%m-%d %H:%M:%S")
-    end = fields.DateTime("%Y-%m-%d %H:%M:%S")
-    projectID = fields.UUID()
+    start = fields.DateTime(DATE_FORMAT_STRING)
+    end = fields.DateTime(DATE_FORMAT_STRING)
     isAllDay = fields.Boolean()
-    isCompleted = fields.Boolean()
+    resource = fields.Nested(ResourceSchema)
+
+    @post_load
+    def processInputData(self, data: dict, **kwargs):
+        """Expand the nested resource field to allow for easy db interaction."""
+        resource = data["resource"]
+        data.pop("resource")
+        data.update(resource)
+        return data
+
+    @post_dump(pass_original=True)
+    def processOutputData(self, data, original_data, **kwargs):
+        """Add taskID, projectID, isCompleted attributes of a task to a nested resource object"""
+        resource = {
+            "taskID": original_data.taskID,
+            "projectID": original_data.projectID,
+            "isCompleted": original_data.isCompleted,
+        }
+
+        data["resource"] = resource
+        return data
