4ee73d88c842141f7632f1687f432ef8984c9dea
Oscar <ocisneros1@csu.fullerton.edu>
2022-11-25 22:22:42 -0800

    prototype for projectt containers

--- client/src/pages/Projects/index.tsx
+++ client/src/pages/Projects/index.tsx
@@ -1,4 +1,4 @@
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { nanoid } from "@reduxjs/toolkit";
 import { useState } from "react";
@@ -11,47 +11,58 @@ export default function Projects() {
     projectID: nanoid(8),
     name: "No Projects",
     taskList: Tasks,
-    importantDates: new Array<ImportantDate>,
-    deadline: "none"
+    importantDates: new Array<ImportantDate>(),
+    deadline: "none",
   };
 
   function calculateCompleted(): number {
     let totalCompleted = 0;
     for (const task of Tasks) {
-      if (task.resource && task.resource.isComplete)
-        totalCompleted++;
-    }    
+      if (task.resource && task.resource.isComplete) totalCompleted++;
+    }
     return totalCompleted;
   }
-  const [taskProgress, setTaskProgress] = useState((): number =>{
+  const [taskProgress, setTaskProgress] = useState((): number => {
     return calculateCompleted() / Tasks.length;
   });
   const [showMilestones, setShowMilestones] = useState(false);
-  const [showTasks, setShowTasks] = useState(false);
+  const [showTasks, setShowTasks] = useState(true);
 
   return (
     <div className="p-4 grid grid-cols-3 h-fit">
-      <div className="p-4 rounded-md text-white bg-green-500 h-36 shadow-2xl">
+      <div className="p-4 rounded-md text-white bg-slate-700 shadow-2xl">
         <h2 className="font-bold text-center mb-2">{project.name}</h2>
         <section className="grid grid-cols-3">
           <h3>Progress:</h3>
           <div className="col-span-2 text-right px-6">{taskProgress}</div>
         </section>
         <section className="grid grid-cols-3">
-          <h3 className="col-span-2">
-            Milestones: 
-          </h3>
+          <h3 className="col-span-2">Milestones:</h3>
           <button className="text-right px-6">
             <FontAwesomeIcon icon={faAngleDown} />
           </button>
         </section>
         <section className="grid grid-cols-3">
-        <h3 className="col-span-2">
-            Tasks: 
-          </h3>
-          <button className="text-right px-6">
-            <FontAwesomeIcon icon={faAngleDown} />
+          <h3 className="col-span-2">Tasks:</h3>
+          <button
+            className="text-right px-6"
+            onClick={() => setShowTasks(!showTasks)}
+          >
+            {showTasks ? (
+              <FontAwesomeIcon icon={faAngleUp} />
+            ) : (
+              <FontAwesomeIcon icon={faAngleDown} />
+            )}
           </button>
+          {showTasks ? (
+            <div className="col-span-3 p-2">
+              {Tasks.map((task) => {
+                return <div className="p-2 font-bold mb-1 bg-black rounded-md">{task.title}</div>;
+              })}
+            </div>
+          ) : (
+            <br />
+          )}
         </section>
       </div>
     </div>
